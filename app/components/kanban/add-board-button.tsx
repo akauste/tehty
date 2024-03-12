f0909d8d1116c6576a3c56dac05b76dae742fd59
Aku Kauste <aku.kauste@gmail.com>
2024-03-12 16:09:49 +0200

    Add board button

---
+++ app/components/kanban/add-board-button.tsx
@@ -0,0 +1,44 @@
+import { Dispatch, useState } from "react";
+import BoardEdit from "./board-edit";
+import { KanbanActions } from "./kanban";
+import { Board } from "@/lib/db";
+
+const AddBoardButton = ({
+  user_id,
+  dispatch,
+}: {
+  user_id: string;
+  dispatch: Dispatch<KanbanActions>;
+}) => {
+  const [showEdit, setShowEdit] = useState(false);
+
+  const addBoard = (board: Board) => dispatch({ type: "add-board", board });
+
+  return (
+    <>
+      <button
+        className=" px-1 border border-slate-500 rounded hover:bg-slate-200 hover:text-sky-800"
+        onClick={() => setShowEdit((s) => !s)}
+      >
+        + Add board
+      </button>
+      {showEdit && (
+        <BoardEdit
+          board={{
+            board_id: Math.ceil(Math.random() * 10000),
+            user_id: "",
+            orderno: 0,
+            name: "",
+            backgroundColor: "",
+            show: true,
+            showDoneTasks: true,
+            tasks: [],
+          }}
+          update={addBoard}
+          close={() => setShowEdit(false)}
+        />
+      )}
+    </>
+  );
+};
+export default AddBoardButton;
