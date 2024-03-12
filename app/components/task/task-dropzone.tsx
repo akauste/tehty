322b08685520640c3e3a9b47590cde0fe061a4a6
Aku Kauste <aku.kauste@gmail.com>
2024-03-12 17:07:15 +0200

    fix dropzone typing

--- app/components/task/task-dropzone.tsx
+++ app/components/task/task-dropzone.tsx
@@ -1,17 +1,17 @@
 import { useDrop } from "react-dnd";
-import { Task } from "../kanban/board-list";
+import { Task } from "@/lib/db";
 import { Dispatch } from "react";
 import { KanbanActions } from "../kanban/kanban";
 
 interface TaskDropzoneProps {
-  board_id: Number
-  dispatch: Dispatch<KanbanActions>
+  board_id: number;
+  dispatch: Dispatch<KanbanActions>;
 }
 
-const TaskDropzone = ({board_id, dispatch}: TaskDropzoneProps) => {
+const TaskDropzone = ({ board_id, dispatch }: TaskDropzoneProps) => {
   const [, drop] = useDrop(
     () => ({
-      accept: 'task',
+      accept: "task",
       /*hover({ id: draggedId, task: sourceTask }: {id: Number, originalIndex: number, task: Task, removeOld: (id: Number) => void}) {
         if(board_id == sourceTask.category) {
           const { index: overIndex } = find(task.task_id)
@@ -22,16 +22,23 @@ const TaskDropzone = ({board_id, dispatch}: TaskDropzoneProps) => {
           console.log('HOVER OVER OTHER CATEGORY src/tgt:', sourceTask.category, task.category);
         }
       },*/
-      drop({id: draggedId, task: sourceTask}: {id: Number, task: Task}) {
-        if(board_id != sourceTask.board_id) {
-          dispatch({type: 'append-remove-task', board_id, task: sourceTask});
+      drop({ id: draggedId, task: sourceTask }: { id: number; task: Task }) {
+        if (board_id != sourceTask.board_id) {
+          dispatch({ type: "append-remove-task", board_id, task: sourceTask });
         }
-      }
+      },
     }),
     []
-  )
+  );
 
-  return <li ref={(node) => drop(node)} className="p-8 bg-sky-200 opacity-50 hover:opacity-100">Drop task here</li>;
-}
+  return (
+    <li
+      ref={(node) => drop(node)}
+      className="p-8 bg-sky-200 opacity-50 hover:opacity-100"
+    >
+      Drop task here
+    </li>
+  );
+};
 
 export default TaskDropzone;
