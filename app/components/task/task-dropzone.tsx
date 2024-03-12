091aed0d14b89d2165c13c4a6175e281980dc2ca
Aku Kauste <aku.kauste@gmail.com>
2024-03-12 17:07:49 +0200

    clean comments

--- app/components/task/task-dropzone.tsx
+++ app/components/task/task-dropzone.tsx
@@ -12,16 +12,6 @@ const TaskDropzone = ({ board_id, dispatch }: TaskDropzoneProps) => {
   const [, drop] = useDrop(
     () => ({
       accept: "task",
-      /*hover({ id: draggedId, task: sourceTask }: {id: Number, originalIndex: number, task: Task, removeOld: (id: Number) => void}) {
-        if(board_id == sourceTask.category) {
-          const { index: overIndex } = find(task.task_id)
-          move(draggedId, overIndex)
-          
-        }
-        else {
-          console.log('HOVER OVER OTHER CATEGORY src/tgt:', sourceTask.category, task.category);
-        }
-      },*/
       drop({ id: draggedId, task: sourceTask }: { id: number; task: Task }) {
         if (board_id != sourceTask.board_id) {
           dispatch({ type: "append-remove-task", board_id, task: sourceTask });
