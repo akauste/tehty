8c3bd5148a5854643607e7c9780947d9239a787a
Aku Kauste <aku.kauste@gmail.com>
2024-03-15 19:52:22 +0200

    Kanban page loading test now works

--- test/kanban-page.test.tsx
+++ test/kanban-page.test.tsx
@@ -3,9 +3,10 @@ import { render, screen } from "@testing-library/react";
 import Page from "../app/kanban/page";
 
 // Does not pass currently, need to start working this out with something simpler
-test.skip("Page", () => {
-  render(<Page />);
+test("Page", async () => {
+  render(await Page());
   expect(
     screen.getByRole("heading", { level: 2, name: "Kanban" })
   ).toBeDefined();
+  expect(screen.getByRole("heading", { name: "Kanban test" })).toBeDefined();
 });
