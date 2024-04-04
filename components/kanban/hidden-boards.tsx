7ab7dc8505b0969b84ff25b217210233ea12bbff
Aku Kauste <aku.kauste@gmail.com>
2024-04-04 15:49:47 +0300

    Fix html blooper (button inside button)

--- components/kanban/hidden-boards.tsx
+++ components/kanban/hidden-boards.tsx
@@ -11,13 +11,12 @@ const HiddenBoards = ({ hiddenBoards, dispatch }: HiddenBoardsProps) => {
   const [showList, setShowList] = useState(false);
 
   return (
-    <button
-      className="bg-gray-300 border border-gray-600 rounded-full px-1 text-gray-800"
-      onClick={() => setShowList((s) => !s)}
-    >
-      {hiddenBoards.length} hidden boards
+    <span className="bg-gray-300 border border-gray-600 rounded-full px-1 text-gray-800">
+      <button className="py-1" onClick={() => setShowList((s) => !s)}>
+        {hiddenBoards.length} hidden boards
+      </button>
       {showList && (
-        <div className="absolute z-10 -ml-4 mt-2 bg-white p-2 shadow-md shadow-slate-400 min-w-24">
+        <div className="absolute z-10 mt-1 bg-white p-2 shadow-md shadow-slate-400 min-w-24">
           <ul>
             {hiddenBoards.map((b) => (
               <li key={b.board_id}>
@@ -38,7 +37,7 @@ const HiddenBoards = ({ hiddenBoards, dispatch }: HiddenBoardsProps) => {
           </ul>
         </div>
       )}
-    </button>
+    </span>
   );
 };
 export default HiddenBoards;
